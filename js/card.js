4bde9fcb6d44ed3274f6a99451e4539ad4717a23
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-03-08 04:54:44 +0600

    modal section update

--- js/card.js
+++ js/card.js
@@ -8,7 +8,7 @@ const closedBtn = document.getElementById("closed");
 
 // api all data
 const loadCardData = () => {
-    loadingSpinner.classList.remove("hidden");
+  loadingSpinner.classList.remove("hidden");
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((les) => les.json())
     .then((data) => {
@@ -20,7 +20,7 @@ const loadCardData = () => {
 // api search section
 document.getElementById("search-btn").addEventListener("click", () => {
   const searchText = document.getElementById("search-input").value;
-    loadingSpinner.classList.remove("hidden");
+  loadingSpinner.classList.remove("hidden");
   let url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
   fetch(url)
     .then((les) => les.json())
@@ -84,7 +84,7 @@ const displayCard = (eliments) => {
     // card
     let card = document.createElement("div");
     card.innerHTML = `
-        <div
+        <div data-id="${eliment.id}"
             class="shadow-2xl rounded-2xl p-5 space-y-4 border-t-4 ${borderColor}">
             <div class="flex justify-between">
                 ${ststusIcon}
@@ -123,4 +123,53 @@ const displayCard = (eliments) => {
   });
 };
 
+document.getElementById("card-container").addEventListener("click", (event) => {
+  let card = event.target.closest("[data-id]");
+  if (!card) return;
+  const id = card.dataset.id;
+  let url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  fetch(url)
+  .then((res)=> res.json())
+  .then((data) => showModal(data.data));
+});
+// modal
+let showModal = (data) => {
+    const modalCard = document.getElementById("modal-card");
+    modalCard.innerHTML = `
+     <div class="p-5 space-y-5 bg-white">
+          <h1 class="font-bold text-2xl">Fix broken image uploads</h1>
+           <div class="flex items-center gap-4">
+            <p class="btn text-white bg-green-500 rounded-4xl">Opened</p>
+            <p class="text-gray-500">Opened by Fahim Ahmed</p>
+            <p class="text-gray-500">22/02/2026</p>
+           </div>
+           <!-- ... -->
+           <div class="flex gap-3">
+              <h2
+                class="btn rounded-3xl text-[#EF4444] bg-[#FEECEC] border-[#FECACA]"
+              >
+                <i class="fa-solid fa-bug"></i>Bug
+              </h2>
+              <h2
+                class="btn rounded-3xl bg-[#FFF8DB] border-[#FDE68A] text-[#D97706]"
+              >
+                <i class="fa-solid fa-life-ring"></i>HELP WANTED
+              </h2>
+            </div>
+            <!-- .... -->
+            <p class="text-gray-500">The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.</p>
+            <div class="grid grid-cols-2 bg-gray-100 p-5 rounded-2xl">
+              <div>
+                <p class="text-gray-500">Assignee:</p>
+                <h2 class="font-semibold text-xl">Fahim Ahmed</h2>
+              </div>
+              <div>
+                <p class="text-gray-500">Priority:</p>
+                <p class="btn bg-red-500 rounded-4xl px-5  text-white">high</p>
+              </div>
+            </div>
+         </div>
+    `
+    document.getElementById("my_modal_5").showModal();
+};
 loadCardData();
